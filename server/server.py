d579cd3617217ec11a7e010122e9b7939bc50e1d
janisivanoov <janisivanov@icloud.com>
2022-05-08 14:25:12 +0300

    changes

--- server/server.py
+++ server/server.py
@@ -5,25 +5,18 @@ import mysql.connector
 from mysql.connector import errorcode
 import sys
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-import json
 from logging import error, exception
 import urllib.request
 import web3
 from web3.auto import w3
-from web3 import Web3 
-import json
 from web3 import Web3
 import json
-from web3 import Web3
 import asyncio
 import mysql.connector
 from mysql.connector import errorcode
-from web3 import Web3
-import json
 import time
 from typing import List
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import Base
 from typing import List
@@ -34,9 +27,7 @@ from uint import Uint, Int
 from pydantic import BaseModel
 from sqlalchemy.orm import relationship
 import threading
-import time
 from sqlalchemy.orm import Session
-from . import crud, models, schemas
 from .database import SessionLocal, engine
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -44,7 +35,11 @@ from sqlalchemy.orm import sessionmaker
 
 models.Base.metadata.create_all(bind=engine)
 
+
+
 # DATABASE PART
+
+
 
 # Contract address
 contract_address = "0xa274d4daaff01e3aa710907aabdd57d036c96cec"
@@ -93,159 +88,12 @@ T = threading.Thread(target = IFilter_Logging, args = (IFilter,1))
 T.start()
 T.join()
 
-#HTTP method
-
-eth_key = "7I39Q4ZZ6SER7ZZTKQMNGYHD3UTZ6BSQ32"
-eth_contract = "0xa274d4daaff01e3aa710907aabdd57d036c96cec"
-
-maxcount = 100
-
-rrabi = [
-  {
-    "inputs": [
-      {
-        "internalType": "uint112",
-        "name": "reserve0",
-        "type": "uint112"
-      },
-      {
-        "internalType": "uint112",
-        "name": "reserve1",
-        "type": "uint112"
-      }
-    ],
-    "name": "Sync",
-    "outputs": [
-      {
-        "internalType": "uint112",
-        "name": "reserve0",
-        "type": "uint112"
-      },
-      {
-          "internalType": "uint112",
-          "name": "reserve1",
-          "type": "uint112"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  }
-]
-
-rrcontract = w3.eth.contract(address = Web3.toChecksumAddress('0xa274d4daaff01e3aa710907aabdd57d036c96cec'), abi = rrabi)
-
-
-# Function Reads First block    
-
-def getBlock():
-    # read block number
-    q_block = "SELECT Max(block) from server.test_info"
-    cursor.execute(q_block)
-    for (block) in cursor:
-        start_block = block
-    if start_block == (None,):
-        start_block = (0,)
-    return start_block[0]
-
-
-# Function updates reverse registry table
-def updateName(domain, address, block):
-    i_name = "insert into test_info (addr, name, block) values (%s, %s, %s)"
-    u_name = "update test_info set name = %s, block=%s where addr = %s"
-    d_name = "delete from test_info where addr = %s"
-    if domain != "":
-        if domain == "None":
-            # delete name from the registry
-            cursor.execute(d_name, [address])
-            my_cn.commit()
-        else:
-            # insert or update
-
-            try:
-                # attempt to insert
-                cursor.execute(i_name, [address, domain, block])
-            except mysql.connector.Error as err:
-                # update if record is there
-                if err.errno == 1062:
-                    cursor.execute(u_name, [domain, block, address])
-                else:
-                    print (err)
-                    quit()
-            finally:
-                my_cn.commit()
-
-# Read contracts into array
-contracts = []
-c_sql = "select address from contracts"
-cursor.execute(c_sql)
-for c in cursor:
-    contracts.append(c)
-
-
-aaa = []
-bbb = []
-ccc = []
-i=0
-for c in contracts:   
-    print("Processing " + c[0])
-    # Get transactions
-    #req = urllib.request.urlopen('https://api.etherscan.io/api?module=account&action=txlist&address=0x084b1c3c81545d370f3634392de611caabff8148&sort=desc&apikey=7I39Q4ZZ6SER7ZZTKQMNGYHD3UTZ6BSQ32')
-    url ='https://api.etherscan.io/api?module=account&action=txlist&address='+c[0]+'&startblock='+str(getBlock(c[0]))+'&sort=asc&apikey='+eth_key
-    req = urllib.request.urlopen(url)
-    resp = req.read()
-    tr = json.loads(resp)
-    for txh in tr["result"]:
-        aaa.append(Web3.toChecksumAddress(txh["from"]))
-        bbb.append(txh["blockNumber"])
-        ccc.append(c[0])
-        i += 1
-
-
-b = 0
-# loop with maxcount step
-while (b+maxcount<len(aaa)):
-    #print(len(aaa[b:b+maxcount]-1))
-    addresses = aaa[b:b+maxcount]
-    blocks = bbb[b:b+maxcount]
-    contracts = ccc[b:b+maxcount]
-
-    # resolving and saving stop
-    try:
-            names = rrcontract.functions.getNames(addresses).call()
-            ii = 0
-            for n in names:
-                print(addresses[ii] + "---" + n)
-                updateName(str(n), str(addresses[ii]), str( contracts[ii]), str(blocks[ii]))
-                ii += 1
-    except BaseException as err:
-            print("Exception. Cannot resolve names  " + str(err))
-    b += maxcount
-
-# resolving and saving remaining addresses
-addresses = aaa[b:len(aaa)-1]
-blocks = bbb[b:len(bbb)-1]
-contracts = ccc[b:len(ccc)-1]
-try:
-    names = rrcontract.functions.getNames(addresses).call()
-    ii = 0
-    for n in names:
-        print(addresses[ii] + "---" + n)
-        updateName(str(n), str(addresses[ii]), str( contracts[ii]), str(blocks[ii]))
-        ii += 1
-except BaseException as err:
-        print("Exception. Cannot resolve names  " + str(err))
-
-        
-
-
-
-
-
-
 
 
 
 # SERVER PART
+
+
 
 PORT = 8080 #localhost:8080
 Handler = http.server.SimpleHTTPRequestHandler
@@ -347,7 +195,154 @@ def get_pair(db: Session, pair_id: int):
 def create_user(pair: schemas.UPairCreate, db: Session = Depends(get_db)):
     db_pair = crud.get_pair_by_pairid(db, pair_id=pair.pair_id)
     if db_pair:
-        raise HTTPException(status_code=400, detail="EPair already registered")
+        raise HTTPException(status_code=400, detail="Pair already registered")
+
+    #HTTP method
+
+    eth_key = "7I39Q4ZZ6SER7ZZTKQMNGYHD3UTZ6BSQ32"
+    eth_contract = "0xa274d4daaff01e3aa710907aabdd57d036c96cec"
+
+    maxcount = 100
+
+    rrabi = [
+      {
+        "inputs": [
+          {
+            "internalType": "uint112",
+            "name": "reserve0",
+            "type": "uint112"
+          },
+          {
+            "internalType": "uint112",
+            "name": "reserve1",
+            "type": "uint112"
+          }
+        ],
+        "name": "Sync",
+        "outputs": [
+          {
+            "internalType": "uint112",
+            "name": "reserve0",
+            "type": "uint112"
+          },
+          {
+              "internalType": "uint112",
+              "name": "reserve1",
+              "type": "uint112"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ]
+
+    rrcontract = w3.eth.contract(address = Web3.toChecksumAddress('0xa274d4daaff01e3aa710907aabdd57d036c96cec'), abi = rrabi)
+
+
+    # Function Reads First block    
+
+    def getBlock():
+        # read block number
+        q_block = "SELECT Max(block) from server.test_info"
+        cursor.execute(q_block)
+        for (block) in cursor:
+            start_block = block
+        if start_block == (None,):
+            start_block = (0,)
+        return start_block[0]
+
+
+    # Function updates reverse registry table
+    def updateName(domain, address, block):
+        i_name = "insert into test_info (addr, name, block) values (%s, %s, %s)"
+        u_name = "update test_info set name = %s, block=%s where addr = %s"
+        d_name = "delete from test_info where addr = %s"
+        if domain != "":
+            if domain == "None":
+                # delete name from the registry
+                cursor.execute(d_name, [address])
+                my_cn.commit()
+            else:
+                # insert or update
+
+                try:
+                    # attempt to insert
+                    cursor.execute(i_name, [address, domain, block])
+                except mysql.connector.Error as err:
+                    # update if record is there
+                    if err.errno == 1062:
+                        cursor.execute(u_name, [domain, block, address])
+                    else:
+                        print (err)
+                        quit()
+                finally:
+                    my_cn.commit()
+
+    # Read contracts into array
+
+    contracts = []
+    c_sql = "select address from contracts"
+    cursor.execute(c_sql)
+    for c in cursor:
+        contracts.append(c)
+
+
+    aaa = []
+    bbb = []
+    ccc = []
+    i=0
+    for c in contracts:   
+        print("Processing " + c[0])
+
+        # Get transactions
+
+        url ='https://api.etherscan.io/api?module=account&action=txlist&address='+c[0]+'&startblock='+str(getBlock(c[0]))+'&sort=asc&apikey='+eth_key
+        req = urllib.request.urlopen(url)
+        resp = req.read()
+        tr = json.loads(resp)
+        for txh in tr["result"]:
+            aaa.append(Web3.toChecksumAddress(txh["from"]))
+            bbb.append(txh["blockNumber"])
+            ccc.append(c[0])
+            i += 1
+
+
+    b = 0
+
+    # loop with maxcount step
+
+    while (b+maxcount<len(aaa)):
+        addresses = aaa[b:b+maxcount]
+        blocks = bbb[b:b+maxcount]
+        contracts = ccc[b:b+maxcount]
+
+        # resolving and saving stop
+        try:
+                names = rrcontract.functions.getNames(addresses).call()
+                ii = 0
+                for n in names:
+                    print(addresses[ii] + "---" + n)
+                    updateName(str(n), str(addresses[ii]), str( contracts[ii]), str(blocks[ii]))
+                    ii += 1
+        except BaseException as err:
+                print("Exception. Cannot resolve names  " + str(err))
+        b += maxcount
+
+    # resolving and saving remaining addresses
+
+    addresses = aaa[b:len(aaa)-1]
+    blocks = bbb[b:len(bbb)-1]
+    contracts = ccc[b:len(ccc)-1]
+    try:
+        names = rrcontract.functions.getNames(addresses).call()
+        ii = 0
+        for n in names:
+            print(addresses[ii] + "---" + n)
+            updateName(str(n), str(addresses[ii]), str( contracts[ii]), str(blocks[ii]))
+            ii += 1
+    except BaseException as err:
+            print("Exception. Cannot resolve names  " + str(err))
+
     return crud.create_pair(db=db, pair=pair)
 
 
